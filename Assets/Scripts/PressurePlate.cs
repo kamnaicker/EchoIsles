edb41b7c440bfa009d88f4a78d3d17d3a6e2d7bd
Vladimir Trifautan <43037076+TeaPot98@users.noreply.github.com>
2026-02-19 21:28:18 +0200

    Scrum 12 door (#22)
    * SCRUM-12: Add door and pressure plate

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -11,25 +11,21 @@ public class PressurePlate : InteractiveObjectTrigger
 
 	private void Awake()
 	{
-		_renderer = model.GetComponent<Renderer>();
-		_defaultMaterial = _renderer.material;
+		// _renderer = model.GetComponent<Renderer>();
+		// _defaultMaterial = _renderer.material;
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.CompareTag("Player"))
-		{
-			_renderer.material = pressedMaterial;
+			// _renderer.material = pressedMaterial;
 			Activate();
-		}
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
 		if (other.gameObject.CompareTag("Player"))
-		{
-			_renderer.material = _defaultMaterial;
+			// _renderer.material = _defaultMaterial;
 			Deactivate();
-		}
 	}
 }
