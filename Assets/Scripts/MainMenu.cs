2def5b71937f28464adc623703ddcd474acd3ac0
TeaPot98 <trifautan@gmail.com>
2026-02-28 16:52:51 +0200

    replace greyboxing assets with final ones

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -21,8 +21,7 @@ public class MainMenu : MonoBehaviour
 
 	private void StartGame(ClickEvent evt)
 	{
-		// TODO: Replace with the Main Level scene index
-		SceneManager.LoadScene(0);
+		SceneManager.LoadScene(1);
 	}
 
 	private void QuitGame(ClickEvent evt)
